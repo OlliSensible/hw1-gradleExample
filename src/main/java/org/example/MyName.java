94bf42072f65e259c56ae78217c9e5ea4d3531e0
OlliSensible <127432509+OlliSensible@users.noreply.github.com>
2023-10-04 06:45:42 +0200

    without myname.jar

---
+++ src/main/java/org/example/MyName.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+import java.util.Map;
+
+public class MyName {
+    public static void main(String[] args) {
+        Map<String, String> map = Map.of("name", "Olha", "lastName", "Senchyk");
+
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(map));
+    }
+}
